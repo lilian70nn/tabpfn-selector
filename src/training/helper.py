e2fd2b10435ec7e2e98475bac411fd0acc6824d3
lilian70nn <lilian70nn@gmail.com>
2026-08-12 22:03:53 +0200

    test

--- src/training/helper.py
+++ src/training/helper.py
@@ -1,5 +1,6 @@
 from dataclasses import fields
 import torch
+from functools import partial
 
 def move_batch_to_device(batch, device):
     kwargs = {}
@@ -11,10 +12,20 @@ def move_batch_to_device(batch, device):
             kwargs[f.name] = v
     return type(batch)(**kwargs)
 
+# def infer_loader_use_selector(loader):
+#     batch = next(iter(loader))
+#     assert hasattr(batch, "use_selector"), (
+#         "Batch must contain use_selector. "
+#         "Set batch.use_selector inside collate_tasks."
+#     )
+#     return bool(batch.use_selector)
 def infer_loader_use_selector(loader):
-    batch = next(iter(loader))
-    assert hasattr(batch, "use_selector"), (
-        "Batch must contain use_selector. "
-        "Set batch.use_selector inside collate_tasks."
+    collate_fn = loader.collate_fn
+
+    if isinstance(collate_fn, partial):
+        if "use_selector" in collate_fn.keywords:
+            return bool(collate_fn.keywords["use_selector"])
+
+    raise ValueError(
+        "Cannot infer use_selector from loader.collate_fn."
     )
-    return bool(batch.use_selector)
