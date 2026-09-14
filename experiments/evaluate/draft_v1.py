16186193d8e4b75615863d4e4559202c4a7df241
lilian70nn <lilian70nn@gmail.com>
2026-09-15 00:08:05 +0200

    update

--- experiments/evaluate/draft_v1.py
+++ experiments/evaluate/draft_v1.py
@@ -461,4 +461,4 @@ if __name__ == "__main__":
     )
 
     model_path = "/content/best_ckpt.pt"
-    main(model=model, model_path=model_path, task_kind="classification", datasets=CLS_DATASETS, evaluate_importance=True)
+    main(model=model, model_path=model_path, task_kind="classification", datasets=CLS_DATASETS, evaluate_importance=False)
